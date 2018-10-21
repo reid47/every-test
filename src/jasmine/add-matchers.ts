cc96107a77354b274140d7ca687bcb1ab87a4910
Reid Mitchell <reidmitchell1@gmail.com>
2018-10-21 02:54:52 -0700

    Add types for Jasmine matchers

--- src/jasmine/add-matchers.ts
+++ src/jasmine/add-matchers.ts
@@ -1,3 +1,22 @@
 import { matchers } from './index';
 
+declare global {
+  namespace jasmine {
+    interface Matchers<T> {
+      toBeChecked: () => T;
+      toBeDisabled: () => T;
+      toBeEmpty: () => T;
+      toBeFocused: () => T;
+      toBeHidden: () => T;
+      toContainText: (text: string | RegExp) => T;
+      toExist: () => T;
+      toHaveAttribute: (attr: string, value?: string) => T;
+      toHaveClass: (...classes: string[]) => T;
+      toHaveTagName: (tagName: string) => T;
+      toHaveText: (text: string) => T;
+      toHaveValue: (value: any) => T;
+    }
+  }
+}
+
 jasmine.addMatchers(matchers);
